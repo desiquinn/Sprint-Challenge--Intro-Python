b1c1b837680247bd3f9223a72760722b0904ee26
Desiree Morris <desiquinncompany@gmail.com>
2019-12-06 14:25:55 -0500

    completed oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is the BaseClass
+# Two Types of Vehicles 1. Flight Vehicle 2. Ground Vehicle Both should inherit Vehicle
+# Two Types of FlightVehicles 1. Starship 2. Airplane Both should inherit FlightVehicle
+# Two Types of GroundVehicles 1. Car 2. Motorscycle Both should inherit GroundVehicle
+
+class Vehicle:
+    # base class
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
